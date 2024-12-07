3cfdef039545b6835df2a0fcbcbba2b0b0f04b3c
raitonoberu <raitonoberu@mail.ru>
2024-12-07 18:23:09 +0500

    fix: read .fish_profile after declaring add_path

--- dot_config/fish/general.fish
+++ dot_config/fish/general.fish
@@ -12,16 +12,16 @@ if [ "$XDG_SESSION_TYPE" = wayland ]
     set -gx ELM_ENGINE wayland_egl
 end
 
-# apply .profile: use this to put fish compatible .profile stuff in
-if test -f ~/.fish_profile
-    source ~/.fish_profile
-end
-
 # declarative alternative to fish_add_path
 function add_path --argument path
     if test -d $path && not contains -- $path $PATH
         set -p PATH $path
     end
+end
+
+# apply .profile: use this to put fish compatible .profile stuff in
+if test -f ~/.fish_profile
+    source ~/.fish_profile
 end
 
 add_path ~/.local/bin
